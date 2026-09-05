91b7be4b5d8ced4a694ccdc623f894fa7514f0fb
Yarchik <yaroslav777007@gmail.com>
2026-09-05 16:58:17 +0500

    Create Theme.kt

---
+++ app/src/main/java/com/manager/Theme.kt
@@ -0,0 +1,34 @@
+package com.manager
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val LightColors = lightColorScheme()
+private val DarkColors = darkColorScheme()
+
+@Composable
+fun FileManagerTheme(
+    content: @Composable () -> Unit
+) {
+    val context = LocalContext.current
+    val darkTheme = isSystemInDarkTheme()
+
+    val colorScheme = when {
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && darkTheme -> dynamicDarkColorScheme(context)
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> dynamicLightColorScheme(context)
+        darkTheme -> DarkColors
+        else -> LightColors
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content
+    )
+}
